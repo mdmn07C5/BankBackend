5c40291de18ea96911c8d6a892c88b6a53ef10b5
mdmn07C5 <malicdan@protonmail.ch>
2023-11-06 15:25:15 -0500

    changed db connection object to be global so it can be accessible in other tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,14 +15,16 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	dbconnection, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
 	}
 
-	testQueries = New(dbconnection)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
