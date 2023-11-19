0372c523d85e51fe248c380f8ff12363574bc627
mdmn07C5 <malicdan@protonmail.ch>
2023-11-19 16:28:55 -0500

    added one route

--- api/server.go
+++ api/server.go
@@ -11,13 +11,23 @@ type Server struct {
 	router *gin.Engine // routes each API request to the correct handler for processing
 }
 
+// Start runs HTTP server on specified address and starts listening to API requests
+func (s *Server) Start(address string) error {
+	return s.router.Run(address)
+}
+
 // NewServer creates a new HTTP server instance and setup routing
 func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
 	// TODO: add routes to router
+	router.POST("/accounts", server.createAccount) // if multiple handlers are passed, the last one is the real handler - the rest are middleware
 
 	server.router = router
 	return server
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
