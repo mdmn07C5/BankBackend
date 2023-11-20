7a85af6b714937635ccf44366697345070292221
mdmn07C5 <malicdan@protonmail.ch>
2023-11-20 16:19:44 -0500

    moved config variables to a config file

--- main.go
+++ main.go
@@ -7,24 +7,23 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/mdmn07C5/bank/api"
 	db "github.com/mdmn07C5/bank/db/sqlc"
-)
-
-// need to connect to db and create store first
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/mdmn07C5/bank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("unable to load configuration:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
---
+++ util/config.go
@@ -0,0 +1,29 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config holds all configuration variables of the application.
+// Values are read by viper from a config file or environment variables
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reds configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env") // maybe change to json later
+
+	// override values read from config file if they exist in env
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
