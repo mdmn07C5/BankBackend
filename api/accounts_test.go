01f249bb5282103d09f9e99437aa9172e54afb06
mdmn07C5 <malicdan@protonmail.ch>
2023-11-21 19:51:05 -0500

    added test for create account API

--- api/accounts_test.go
+++ api/accounts_test.go
@@ -110,6 +110,89 @@ func TestGetAccountAPI(t *testing.T) {
 	}
 }
 
+func TestCreateAccountAPI(t *testing.T) {
+	account := randomAccount()
+	accountCreatedParams := db.CreateAccountParams{
+		Owner:    account.Owner,
+		Currency: account.Currency,
+		Balance:  0,
+	}
+
+	testCases := []struct {
+		name          string
+		accountParams db.CreateAccountParams
+		buildStubs    func(mockStore *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:          "OK",
+			accountParams: accountCreatedParams,
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(accountCreatedParams)).
+					Times(1).
+					Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccount(t, recorder.Body, account)
+			},
+		},
+		{
+			name:          "InternalError",
+			accountParams: accountCreatedParams,
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(accountCreatedParams)).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidRequestParams",
+			accountParams: db.CreateAccountParams{
+				Owner:    account.Owner,
+				Currency: "LIR",
+				Balance:  0,
+			},
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			mockStore := mockdb.NewMockStore(ctrl)
+
+			tc.buildStubs(mockStore)
+
+			server := NewServer(mockStore)
+			recorder := httptest.NewRecorder()
+
+			jsonBytes, err := json.Marshal(tc.accountParams)
+
+			request, err := http.NewRequest(http.MethodPost, "/accounts", bytes.NewReader(jsonBytes))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
 func randomAccount() db.Account {
 	return db.Account{
 		ID:       util.RandomInt(1, 1000),
