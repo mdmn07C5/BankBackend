6493034e8d07c6733e4f2bd253b683ba5e17fa9a
mdmn07C5 <malicdan@protonmail.ch>
2023-12-12 22:08:48 -0500

    added sessionMiddleware tests

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,13 +1,18 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	mockdb "github.com/mdmn07C5/bank/db/mock"
+	db "github.com/mdmn07C5/bank/db/sqlc"
 	"github.com/mdmn07C5/bank/token"
 	"github.com/mdmn07C5/bank/util"
 	"github.com/stretchr/testify/require"
@@ -103,7 +108,104 @@ func TestAuthMiddleWare(t *testing.T) {
 
 			tc.setupAuth(t, request, server.tokenMaker)
 			server.router.ServeHTTP(recorder, request)
+			fmt.Println(request)
 			tc.checkResponse(t, recorder)
 		})
 	}
+}
+
+func TestSessionMiddleWare(t *testing.T) {
+	user, _ := randomUser(t)
+
+	testCases := []struct {
+		name          string
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		setupSession  func(t *testing.T, request *http.Request, server *Server) db.CreateSessionParams
+		buildStubs    func(mockstore *mockdb.MockStore, params db.CreateSessionParams, session db.Session)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
+			setupSession: func(t *testing.T, request *http.Request, server *Server) db.CreateSessionParams {
+				authHeader := request.Header.Get(authorizationHeaderKey)
+				require.NotEmpty(t, authHeader)
+
+				fields := strings.Fields(authHeader)
+				require.GreaterOrEqual(t, len(fields), 2)
+
+				refreshTokenPayload, err := server.tokenMaker.VerifyToken(fields[1])
+				require.NoError(t, err)
+				require.NotEmpty(t, refreshTokenPayload)
+
+				arg := db.CreateSessionParams{
+					ID:           refreshTokenPayload.ID,
+					Username:     user.Username,
+					RefreshToken: fields[1],
+					UserAgent:    "test",
+					ClientIp:     "test",
+					IsBlocked:    false,
+					ExpiresAt:    time.Now().Add(time.Minute),
+				}
+				return arg
+			},
+			buildStubs: func(mockstore *mockdb.MockStore, params db.CreateSessionParams, session db.Session) {
+				mockstore.EXPECT().
+					CreateSession(gomock.Any(), gomock.Eq(params)).
+					Times(1).
+					Return(session, nil)
+
+				mockstore.EXPECT().
+					GetSession(gomock.Any(), gomock.Eq(session.ID)).
+					Times(1).
+					Return(session, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			server := newTestServer(t, store)
+			recorder := httptest.NewRecorder()
+
+			authPath := "/auth"
+			server.router.GET(
+				authPath,
+				authMiddleware(server.tokenMaker),
+				sessionMiddleWare(server),
+				func(ctx *gin.Context) {
+					ctx.JSON(http.StatusOK, gin.H{})
+				},
+			)
+			request, err := http.NewRequest(http.MethodGet, authPath, nil)
+			require.NoError(t, err)
+
+			tc.setupAuth(t, request, server.tokenMaker)
+
+			arg := tc.setupSession(t, request, server)
+
+			var session db.Session
+			tc.buildStubs(store, arg, session)
+
+			session, err = server.store.CreateSession(context.Background(), arg)
+
+			server.store.GetSession(context.Background(), session.ID)
+
+			server.router.ServeHTTP(recorder, request)
+			fmt.Println(request)
+			// tc.checkResponse(t, recorder)
+		})
+	}
+
 }
