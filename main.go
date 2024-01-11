413d5455f3c948d4bf07ea98d2ec170c80ac7d3d
mdmn07C5 <mdmn07C5@users.noreply.github.com>
2024-01-11 14:08:53 -0500

    added swagger documentation (#38)

--- main.go
+++ main.go
@@ -71,6 +71,9 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", gRPCMux)
 
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot create net.Listener:", err)
