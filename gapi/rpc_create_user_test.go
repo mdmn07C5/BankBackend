7deb8c66345a7b0744045ddbf8c6126bf6c23f3e
mdmn07C5 <malicdan@protonmail.ch>
2024-01-15 15:55:34 -0500

    added test for rpc CreateUser

---
+++ gapi/rpc_create_user_test.go
@@ -0,0 +1,179 @@
+package gapi
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/lib/pq"
+	mockdb "github.com/mdmn07C5/bank/db/mock"
+	db "github.com/mdmn07C5/bank/db/sqlc"
+	"github.com/mdmn07C5/bank/pb"
+	"github.com/mdmn07C5/bank/util"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type eqCreateUserParamsMatcher struct {
+	arg           db.CreateUserParams
+	nakedPassword string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x interface{}) bool {
+	// In case, some value is nil
+	arg, ok := x.(db.CreateUserParams)
+	if !ok {
+		return false
+	}
+
+	err := util.CheckPassword(e.nakedPassword, arg.HashedPassword)
+	if err != nil {
+		return false
+	}
+
+	e.arg.HashedPassword = arg.HashedPassword
+	return reflect.DeepEqual(e.arg, arg)
+
+}
+
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("matches arg %v and password %v", e.arg, e.nakedPassword)
+}
+
+func EqCreateUserParams(arg db.CreateUserParams, nakedPassword string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg, nakedPassword}
+}
+
+func randomUser(t *testing.T) (user db.User, password string) {
+	password = util.RandomString(8)
+	hashedPassword, err := util.HashPassword(password)
+	require.NoError(t, err)
+
+	user = db.User{
+		Username:       util.RandomOwner(),
+		HashedPassword: hashedPassword,
+		FullName:       util.RandomOwner(),
+		Email:          util.RandomEmail(),
+	}
+	return
+
+}
+
+func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user db.User) {
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotUser db.User
+	err = json.Unmarshal(data, &gotUser)
+
+	require.NoError(t, err)
+	require.Equal(t, user.Username, gotUser.Username)
+	require.Equal(t, user.FullName, gotUser.FullName)
+	require.Equal(t, user.Email, gotUser.Email)
+	require.Empty(t, gotUser.HashedPassword)
+}
+
+func TestCreateUserAPI(t *testing.T) {
+	user, password := randomUser(t)
+
+	testCases := []struct {
+		name          string
+		req           *pb.CreateUserRequest
+		buildStubs    func(mockStore *mockdb.MockStore)
+		checkResponse func(t *testing.T, res *pb.CreateUserResponse, err error)
+	}{
+		{
+			name: "OK",
+			req: &pb.CreateUserRequest{
+				Username: user.Username,
+				Password: password,
+				FullName: user.FullName,
+				Email:    user.Email,
+			},
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				userParams := db.CreateUserParams{
+					Username: user.Username,
+					FullName: user.FullName,
+					Email:    user.Email,
+				}
+				mockStore.EXPECT().
+					CreateUser(gomock.Any(), EqCreateUserParams(userParams, password)).
+					Times(1).
+					Return(user, nil)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateUserResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				createdUser := res.GetUser()
+				require.Equal(t, user.Username, createdUser.Username)
+				require.Equal(t, user.FullName, createdUser.FullName)
+				require.Equal(t, user.Email, createdUser.Email)
+			},
+		},
+		{
+			name: "Internal Error",
+			req: &pb.CreateUserRequest{
+				Username: user.Username,
+				Password: password,
+				FullName: user.FullName,
+				Email:    user.Email,
+			},
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(user, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateUserResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.Equal(t, codes.Internal, st.Code())
+				require.True(t, ok)
+			},
+		},
+		{
+			name: "User Already Exists",
+			req: &pb.CreateUserRequest{
+				Username: user.Username,
+				Password: password,
+				FullName: user.FullName,
+				Email:    user.Email,
+			},
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(user, &pq.Error{Code: util.UniqueViolation})
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateUserResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.Equal(t, codes.AlreadyExists, st.Code())
+				require.True(t, ok)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			mockStore := mockdb.NewMockStore(ctrl)
+
+			tc.buildStubs(mockStore)
+
+			server := newTestServer(t, mockStore)
+			res, err := server.CreateUser(context.Background(), tc.req)
+			tc.checkResponse(t, res, err)
+		})
+	}
+}
