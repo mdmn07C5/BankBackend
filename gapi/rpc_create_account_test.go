e9094ca35980104aec0c40f19316429bb208a6af
mdmn07C5 <mdmn07C5@users.noreply.github.com>
2024-01-16 12:59:47 -0500

    added tests for grpc api create account (#46)

---
+++ gapi/rpc_create_account_test.go
@@ -0,0 +1,172 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	mockdb "github.com/mdmn07C5/bank/db/mock"
+	db "github.com/mdmn07C5/bank/db/sqlc"
+	"github.com/mdmn07C5/bank/token"
+	"github.com/mdmn07C5/bank/util"
+
+	"github.com/mdmn07C5/bank/pb"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+func randomAccount(owner string) db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    owner,
+		Balance:  util.RandomAmount(),
+		Currency: util.RandomCurrency(),
+	}
+}
+
+func TestCreateAccountAPI(t *testing.T) {
+	user, _ := randomUser(t)
+	account := randomAccount(user.Username)
+
+	testCases := []struct {
+		name          string
+		req           *pb.CreateAccountRequest
+		buildStubs    func(store *mockdb.MockStore)
+		buildContext  func(t *testing.T, tokenMaker token.Maker) context.Context
+		checkResponse func(t *testing.T, res *pb.CreateAccountResponse, err error)
+	}{
+		{
+			name: "OK",
+			req: &pb.CreateAccountRequest{
+				Currency: account.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.CreateAccountParams{
+					Owner:    account.Owner,
+					Currency: account.Currency,
+					Balance:  0,
+				}
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(account, nil)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				accessToken, _, err := tokenMaker.CreateToken(user.Username, time.Minute)
+				require.NoError(t, err)
+				bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+				md := metadata.MD{
+					authorizationHeader: []string{
+						bearerToken,
+					},
+				}
+				return metadata.NewIncomingContext(context.Background(), md)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateAccountResponse, err error) {
+				require.NoError(t, err)
+				require.Equal(t, account.Owner, res.Account.Owner)
+				require.Equal(t, account.Currency, res.Account.Currency)
+				require.NotNil(t, res)
+			},
+		},
+		{
+			name: "NoAuthorization",
+			req: &pb.CreateAccountRequest{
+				Currency: account.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return context.Background()
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateAccountResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Unauthenticated, st.Code())
+			},
+		},
+		{
+			name: "InternalError",
+			req: &pb.CreateAccountRequest{
+				Currency: account.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(account, sql.ErrConnDone)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				accessToken, _, err := tokenMaker.CreateToken(user.Username, time.Minute)
+				require.NoError(t, err)
+				bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+				md := metadata.MD{
+					authorizationHeader: []string{
+						bearerToken,
+					},
+				}
+				return metadata.NewIncomingContext(context.Background(), md)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateAccountResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, st.Code())
+			},
+		},
+		{
+			name: "InvalidCurrency",
+			req: &pb.CreateAccountRequest{
+				Currency: "NZD",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				accessToken, _, err := tokenMaker.CreateToken(user.Username, time.Minute)
+				require.NoError(t, err)
+				bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+				md := metadata.MD{
+					authorizationHeader: []string{
+						bearerToken,
+					},
+				}
+				return metadata.NewIncomingContext(context.Background(), md)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateAccountResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, st.Code())
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			mockStore := mockdb.NewMockStore(ctrl)
+
+			tc.buildStubs(mockStore)
+			server := newTestServer(t, mockStore)
+
+			ctx := tc.buildContext(t, server.tokenMaker)
+			res, err := server.CreateAccount(ctx, tc.req)
+			tc.checkResponse(t, res, err)
+		})
+	}
+}
