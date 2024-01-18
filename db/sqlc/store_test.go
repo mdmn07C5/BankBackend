995f3079d741cccadfab7d9e68627d959efede83
mdmn07C5 <malicdan@protonmail.ch>
2024-01-18 12:19:01 -0500

    refactor: removed the print statements

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,7 +2,6 @@ package postgresdb
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,7 +12,6 @@ func TestTransferTx(t *testing.T) {
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
-	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
 	n := 10
 	amount := int64(10)
@@ -97,7 +95,6 @@ func TestTransferTx(t *testing.T) {
 		require.Equal(t, toAccount.ID, res.ToAccount.ID)
 
 		// check accounts balance
-		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
 		delta1 := account1.Balance - fromAccount.Balance
 		delta2 := toAccount.Balance - account2.Balance
 		require.Equal(t, delta1, delta2)
@@ -117,8 +114,6 @@ func TestTransferTx(t *testing.T) {
 	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
 	require.NoError(t, err)
 
-	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
-
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 
@@ -129,7 +124,6 @@ func TestTransferTxDeadlock(t *testing.T) {
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
-	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
 	n := 10
 	amount := int64(10)
@@ -166,8 +160,6 @@ func TestTransferTxDeadlock(t *testing.T) {
 
 	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
 	require.NoError(t, err)
-
-	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
 
 	require.Equal(t, account1.Balance, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance, updatedAccount2.Balance)
